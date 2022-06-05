2ea2b5c9bc79eefc1ea38e5f5a9d40e57b283d06
Ko0oT <serkokarjalo@gmail.com>
2022-06-05 13:39:39 +0300

    Упростил функцию проверки комментария

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /*
 Функция, возвращающая случайное целое число из переданного диапазона включительно
 Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+Добавлены условия проверки неотрицательности значений диапазона, а также условие о верхней границе
 */
 
 const getRandomIntInclusive = (min, max) => {
@@ -24,12 +25,6 @@ getRandomIntInclusive(1, 5);
 
 //Функция для проверки максимальной длины строки.
 
-const checkMaxLength = (string, maxLength) => {
-  if (string.length > maxLength) {
-    return false;
-  } else {
-    return true;
-  }
-};
+const checkMaxLength = (string, maxLength) => string.length <= maxLength;
 
 checkMaxLength ('Я сделяль', 140);
