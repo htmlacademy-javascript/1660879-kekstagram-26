95a102c337cf2d7d6b95298d6b34ae6d2a49e05b
Ko0oT <serkokarjalo@gmail.com>
2022-06-11 13:13:00 +0300

    Внес небольшие правки согласно критериям

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/*
-Функция, возвращающая случайное целое число из переданного диапазона включительно
-Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-Добавлены условия проверки неотрицательности значений диапазона, а также условие о верхней границе
-*/
-const getRandomIntInclusive = (min, max) => {
-  if (min < 0 || max < 0 || max <= min) {
-    return -1;
-  } else {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-};
-
-//Функция для проверки максимальной длины строки.
-//Закомментировал, чтобы не ругался линтер, функицю не применял, в условиях не было.
-//const checkMaxLength = (string, maxLength) => string.length <= maxLength;
-
 const COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -31,6 +12,25 @@ const NAMES = ['Милана', 'Владимир', 'Кирилл', 'Виктор
 const PHOTOS_COUNT = 25;
 const COMMENTS_COUNT = 300;
 const AVATARS_COUNT = 6;
+/*
+Функция, возвращающая случайное целое число из переданного диапазона включительно
+Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+Добавлены условия проверки неотрицательности значений диапазона, а также условие о верхней границе
+*/
+const getRandomIntInclusive = (min, max) => {
+  if (min < 0 || max < 0 || max <= min) {
+    return -1;
+  } else {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+};
+/*
+Функция для проверки максимальной длины строки.
+Закомментировал, чтобы не ругался линтер, функицю не применял, в условиях не было.
+const checkMaxLength = (string, maxLength) => string.length <= maxLength;
+*/
 
 //Функция для создания массива от 1 до max
 const getOrderedArray = (max) => {
@@ -41,27 +41,29 @@ const getOrderedArray = (max) => {
   return orderedArray;
 };
 
-const idArr = getOrderedArray(PHOTOS_COUNT);
-const urlArr = getOrderedArray(PHOTOS_COUNT);
-const commentIdArr = getOrderedArray(COMMENTS_COUNT);
+//Создал упорядоченные массивы для нужных полей
+const ids = getOrderedArray(PHOTOS_COUNT);
+const urls = getOrderedArray(PHOTOS_COUNT);
+const commentIds = getOrderedArray(COMMENTS_COUNT);
 
-const getUniqId = (array) => Number(array.splice(getRandomIntInclusive(1, array.length - 1), 1));
+//Функция для выбора случайного числа из созданного массива и "выбрасывания" этого числа из него
+const getUniqId = (arr) => Number(arr.splice(getRandomIntInclusive(1, arr.length - 1), 1));
 
 
 const createPhoto = () => ({
-  id: getUniqId(idArr),
-  url: `photos/${getUniqId(urlArr)}.jpg`,
+  id: getUniqId(ids),
+  url: `photos/${getUniqId(urls)}.jpg`,
   description: 'Еще одна удачная фотография',
   likes: getRandomIntInclusive(15, 200),
   comments: [
     {
-      id: getUniqId(commentIdArr),
+      id: getUniqId(commentIds),
       avatar: `img/avatar-${getRandomIntInclusive(1, AVATARS_COUNT)}.svg`,
       message: COMMENTS[getRandomIntInclusive(0, COMMENTS.length)],
       name: NAMES[getRandomIntInclusive(0, NAMES.length)],
     },
     {
-      id: getUniqId(commentIdArr),
+      id: getUniqId(commentIds),
       avatar: `img/avatar-${getRandomIntInclusive(1, AVATARS_COUNT)}.svg`,
       message: COMMENTS[getRandomIntInclusive(0, COMMENTS.length)],
       name: NAMES[getRandomIntInclusive(0, NAMES.length)],
