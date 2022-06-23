d672386470d2db7cf59b4a8a8cfd1422a523bb9d
Ko0oT <serkokarjalo@gmail.com>
2022-06-24 02:24:48 +0300

    Добавил отрисовку большого изображения

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import './data.js';
-//не испортировал массив similarPhotos, чтобы не ругался линтер, т.к. мы его еще не применяем
-import './render.js';
+import './renderSmall.js';
+import './renderFull.js';
+
